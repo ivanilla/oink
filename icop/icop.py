9ba73a8b43fe0a817848a52e73382b6f9d11dbec
ivanilla <v1perx.819@gmail.com>
2020-12-07 16:54:08 -0800

    mkdir icop

---
+++ icop/icop.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from seleium.webdriver.support.ui import Select
+from request_html import HTMLSession, AsyncHTMLSession
+import time
+
+import configparser
+
+```
+python supreme.py --name="North Face"
+```
+
+base_url = 'https://www.supremenewyork.com'
+
